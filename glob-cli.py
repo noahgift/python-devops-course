33be007dc8990a9a518af2a515b643d2e6c71092
Noah Gift <noah.gift@gmail.com>
2019-11-11 16:12:28 +0000

    adding more functionality

--- glob-cli.py
+++ glob-cli.py
@@ -4,8 +4,12 @@ import glob
 
 
 @click.command()
-def search():
-    results = glob.glob(f"/home/ec2-user/environment/files/*.csv")
+@click.option('--path', prompt='Path to search for csv files',
+              help='This is the path to search for files: /tmp')
+@click.option('--ftype', prompt='Pass in the type of file',
+              help="Pass in the file type:  i.e csv")
+def search(path, ftype):
+    results = glob.glob(f"{path}/*.{ftype}")
     click.echo(f'These are my results: {results}')
 
 if __name__ == '__main__':
