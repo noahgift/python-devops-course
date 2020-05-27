bcd9675cc6e4f0be06b5f36dce308e5db120492e
Noah Gift <noah.gift@gmail.com>
2020-05-27 14:50:54 +0000

    adding marco polo cli

---
+++ marco.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import click
+
+@click.command()
+@click.option('--name',
+              help='Place the name you want to YELL')
+def marco(name):
+    """This is a Marco/Polo game."""
+    
+    if name == "Marco":
+        click.echo("Polo")
+    else:
+        click.echo("No!")
+
+if __name__ == '__main__':
+    marco()
