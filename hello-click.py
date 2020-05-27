6beac833e88f348d88a2a8090267fa4e299a1f31
Noah Gift <noah.gift@gmail.com>
2020-05-27 14:44:52 +0000

    push changes

--- hello-click.py
+++ hello-click.py
@@ -3,6 +3,9 @@ import click
 
 @click.command()
 def hello():
+    """This is a hello world cli
+    To run it type in ./hello-click.py
+    """
     click.echo('Hello World!')
 
 if __name__ == '__main__':
