93d5dd43f9cddd47daef47d5eebee86f952187c0
Noah Gift <noah.gift@gmail.com>
2020-10-08 10:11:52 -0400

    adding local file

---
+++ hello-click.py
@@ -0,0 +1,13 @@
+import click
+
+
+@click.command()
+@click.option("--phrase", prompt="Enter a phrase", help="")
+def tokenize(phrase):
+    """tokenize phrase"""
+
+    click.echo(f"tokenized phrase: {phrase.split()}")
+
+
+if __name__ == "__main__":
+    tokenize()
