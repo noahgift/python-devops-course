0194a6cb145f2b2426d396aca14cba6281f10f8a
Noah Gift <noah.gift@gmail.com>
2021-01-26 22:22:21 -0500

    Add files via upload

---
+++ magic_stuff.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/noahgift/food/master/data/features.en.openfoodfacts.org.products.csv")
+df.drop(["Unnamed: 0", "exceeded", "g_sum", "energy_100g"], axis=1, inplace=True) #drop two rows we don't need
+df = df.drop(df.index[[1,11877]]) #drop outlier
+df.rename(index=str, columns={"reconstructed_energy": "energy_100g"}, inplace=True)
+print(df.head())
